49ce4e97ed25e6c4d682cdd841d8aac75f28db62
Felix Antonelli <felix2.antonelli@live.uwe.ac.uk>
2020-02-17 13:55:57 +0000

    added movement script (not finished)

---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class MovementController : MonoBehaviour
+{
+
+    private Animator anim;
+    private new Rigidbody rigidbody;
+
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
