af351df6278918da61a27ec9dce4d1ce4a4f8b4f
Felix Antonelli <felix2.antonelli@live.uwe.ac.uk>
2020-03-02 13:56:55 +0000

    saved scripts

--- Assets/Scripts/CameraTrigger.cs
+++ Assets/Scripts/CameraTrigger.cs
@@ -17,7 +17,7 @@ public class CameraTrigger : MonoBehaviour
         Collider PlayerCollider = PlayerCharacter.GetComponent<Collider>();
         if (other == PlayerCollider)
         {
-            Vector3 moveCam = new Vector3(liveCam.transform.localPosition.x, liveCam.transform.localPosition.y, liveCam.transform.localPosition.z + 10);
+            Vector3 moveCam = new Vector3(liveCam.transform.localPosition.x, liveCam.transform.localPosition.y, liveCam.transform.localPosition.z - 10);
            /// moveCam = liveCam.transform.TransformDirection(moveCam);
             liveCam.transform.localPosition = moveCam;
         }
