3a55eb27374bbcfe479cc0c156ab5a9ea9e0421f
Felix Antonelli <Felix2.Antonelli@live.uwe.ac.uk>
2020-03-29 23:42:21 +0100

    can now get out of basket

---
+++ Assets/Scripts/GetOutOfBasket.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetOutOfBasket : MonoBehaviour
+{
+
+    private GameObject PlayerCharacter;
+    private Collider PlayerCollider;
+    private Animator animator;
+    private GameObject enviro;
+    public Transform playerTransform;
+
+    private void Awake()
+    {
+        PlayerCharacter = GameObject.FindGameObjectWithTag("Player");
+        PlayerCollider = PlayerCharacter.GetComponent<Collider>();
+        enviro = GameObject.FindGameObjectWithTag("Enviroment");
+        animator = enviro.GetComponent<Animator>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other == PlayerCollider)
+        {
+            if (Input.GetButton("Interact"))
+            {
+                playerTransform.parent = null;
+                playerTransform.position = new Vector3(playerTransform.position.x, playerTransform.position.y, playerTransform.position.z - 5);
+            }
+        }
+    }
+}
